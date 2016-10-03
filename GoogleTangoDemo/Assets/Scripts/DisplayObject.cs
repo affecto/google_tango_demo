11c54743939caa2c55d6cabd17285d39c54961f8
Divinare <niemijoe@hotmail.com>
2016-10-03 16:04:10 +0300

    real camera is now showing, it doesnt draw blue line (the line shows the path we have moved)

--- GoogleTangoDemo/Assets/Scripts/DisplayObject.cs
+++ GoogleTangoDemo/Assets/Scripts/DisplayObject.cs
@@ -21,7 +21,7 @@ public class DisplayObject : MonoBehaviour {
         // Find out if another object is near enough
         // Find out distance between objects
         Vector3 location = transform.position;
-        Vector3 cameraPosition = GameObject.Find("Tango Delta Camera").transform.position;
+        Vector3 cameraPosition = GameObject.Find("Tango AR Camera").transform.position;
         Vector3 difference = location - cameraPosition;
         float distance = difference.magnitude;
         // If distance is smaller than threshold, return True
---
+++ GoogleTangoDemo/Assets/Scripts/HideObject.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HideObject : MonoBehaviour {
+
+    public MeshRenderer rend;
+    
+	// Use this for initialization
+	void Start () {
+        rend = GetComponent<MeshRenderer>();
+        rend.enabled = false;
+
+        // Hide the children of this object
+        MeshRenderer[] rs = gameObject.GetComponentsInChildren<MeshRenderer>();
+        foreach (MeshRenderer r in rs) {
+            r.enabled = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
